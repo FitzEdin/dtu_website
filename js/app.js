796dce1702f742428666f8389e3c09e82f21a9dc
FitzEdin <fitzroy.edinborough@gmail.com>
2021-10-15 15:48:35 -0400

    added supporting css, js, and font files

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var app = angular.module("innovation-hub", []);
+
+app.config(($locationProvider) => {
+	$locationProvider.html5Mode(true);
+});
+
+app.controller("index", ($scope) => {
+	//
+});
---
+++ js/firebase-init.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyAiAUcWqYcbh6KlGVy31pKx34qF47TyUP8",
+	authDomain: "innovation-hub-16e82.firebaseapp.com",
+	databaseURL: "https://innovation-hub-16e82.firebaseio.com",
+	projectId: "innovation-hub-16e82",
+	storageBucket: "innovation-hub-16e82.appspot.com",
+	messagingSenderId: "359382947648"
+};
+firebase.initializeApp(config);
