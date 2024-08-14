30994fc52b76580a9a946cb266f5114356991afb
Fitz Edin <fitzroy.edinborough@gmail.com>
2024-08-14 10:05:39 -0400

    ..update repo with new site design

---
+++ lost.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Lost?</title>
+    <link rel="icon" href="img/favicon.png">
+
+    <meta name="viewport"
+          content="width=device-width,
+                   initial-scale=1">
+    <link href='http://fonts.googleapis.com/css?family=Ubuntu:300' rel='stylesheet' type='text/css'><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body style="font-family: 'Ubuntu', sans-serif;">
+    <?php
+        header("Refresh:3; url=http://muscovado.kn/");
+    ?>
+            <div style="height: 125px;"></div>
+            <div style="text-align: center;" style="width: 66.66666667%; margin-left: 16.66666667%;">
+                <span style="font-size: 85px; color: #36d; height: 105px;">
+                    You seem lost!
+                </span>
+                <hr style="margin-top: 20px; margin-bottom: 20px; border: 0; border-top: 1px solid #eee; height: 0; -moz-box-sizing: content-box; box-sizing: content-box;">
+                <p style="text-align: center;" style="font-size: 25px;">
+                    <i>Lemme walk you to safety.</i>
+                </p>
+            </div>
+</body>
+</html>
